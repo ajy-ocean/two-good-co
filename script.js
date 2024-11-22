2c8808cbeb2cc7efa21c0edaf6941de0ad1493d6
Ajay Rawat <rawat.ajyy@gmail.com>
2024-11-22 15:29:19 +0530

    added animation using gsap

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
 function videoConAnimation() {
   var videoContainer = document.querySelector("#video-container");
   var playBtn = document.querySelector("#play");
@@ -40,3 +45,26 @@ function loadingAnimation() {
 }
 
 loadingAnimation();
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#cursor", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+
+  document.querySelectorAll(".child").forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      gsap.to("#cursor", {
+        transform: "translate(-50%,-50%) scale(1)",
+      });
+    });
+    elem.addEventListener("mouseleave", function () {
+      gsap.to("#cursor", {
+        transform: "translate(-50%,-50%) scale(0)",
+      });
+    });
+  });
+}
+cursorAnimation();
